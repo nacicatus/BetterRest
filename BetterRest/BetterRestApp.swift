41be7a65672c94662e336b9643f1b70a5bfe59bb
SaurabhSikka <drsaurabhsikka@gmail.com>
2024-01-31 13:07:48 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Saurabh Sikka A on 2023. 11. 22..
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
